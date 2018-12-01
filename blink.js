9c2396d330c77770dfa8abaa0f53cae974a8540a
Bruno Barbosa <bbt@cin.ufpe.br>
2018-12-01 18:18:52 -0300

    try using onoff to capacitive sensor

--- blink.js
+++ blink.js
@@ -63,6 +63,15 @@ usonic.init(function (error) {
   }, 1000);
 });
 
+// MARK: Capacitive touch sensor
+var capaciSensor = new Gpio(16, 'in', 'both', {debounceTimeout: 10}); //use GPIO pin 16 as input, and 'both' button presses, and releases should be handled
+capaciSensor.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
+  if (err) { //if an error
+    console.error('There was an error', err); //output error message to console
+    return;
+  }
+  ledGreen.writeSync(value); //turn LED on or off depending on the button state (0 or 1)
+});
 
 
 function unexportOnClose() { //function to run when exiting program
